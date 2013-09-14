c3226519743592361c2660a7bd8666609e96cb4b
Torbjørn Vatn <torbjorn@vatn.org>
2013-09-14 22:42:05 +0200

    Adds a license file

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject usbtesting "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject clj-TEMPer1 "0.1.0-SNAPSHOT"
+  :description "A small clojure program that interfaces with a TEMPer1 device"
+  :license {:name "WTFPL – Do What the Fuck You Want to Public License"
+            :url "http://www.wtfpl.net/"}
   :repositories [["ailis" "http://nexus.ailis.de/content/groups/public"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.codeminders/hidapi "1.1"]]
