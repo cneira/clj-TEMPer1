160c6d777d0638912e4be472faceef7a34d97533
Torbjørn Vatn <torbjorn@vatn.org>
2013-09-23 23:05:07 +0200

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject org.clojars.torbjornvatn/clj-temper1 "0.1.0"
+(defproject org.clojars.torbjornvatn/clj-temper1 "0.1.1-SNAPSHOT"
   :description "A small clojure library that interfaces with a TEMPer1 device"
   :license {:name "WTFPL – Do What the Fuck You Want to Public License"
             :url "http://www.wtfpl.net/"}
   :url "https://github.com/torbjornvatn/clj-TEMPer1"
   :repositories [["ailis" "http://nexus.ailis.de/content/groups/public"]
-                 ["release" {:url "https://clojars.org/repo"
+                 ["releases" {:url "https://clojars.org/repo"
                              :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.torbjornvatn/hidapi "1.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
                  [expectations "1.4.52"]]
-  :test-paths ["test" "src/test/"]
+  :aot [clj-TEMPer1.core]
   :main clj-TEMPer1.core)
